1153e1d38bed0e2dbdeb84f91e5b751bf3cae043
CHarles <calsoufi@microsoft.com>
2023-03-25 00:35:51 +0100

    Full commit and push

---
+++ main.js
@@ -0,0 +1,77 @@
+// main.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-app.js";
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js";
+import { onValue, query } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-database.js';
+
+
+     // Your web app's Firebase configuration
+     const firebaseConfig = {
+        apiKey: "AIzaSyD9p4iFYGtOGmHqPnuzb7kxNgrrNGJ4boo",
+        authDomain: "calsoufiplaces.firebaseapp.com",
+        databaseURL: "https://calsoufiplaces-default-rtdb.europe-west1.firebasedatabase.app/", // Add your Realtime Database URL here
+        projectId: "calsoufiplaces",
+        storageBucket: "calsoufiplaces.appspot.com",
+        messagingSenderId: "804743678576",
+        appId: "1:804743678576:web:0af44414e9180c952582b5"
+    };
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database
+const db = getDatabase(app);
+
+// Add the form event listener
+document.getElementById("updateForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    // Retrieve the input values
+    const city = document.getElementById("city").value;
+    const startdate = document.getElementById("startdate").value;
+
+    // Create a new object with the input values
+    const newPlace = {
+        city: city,
+        startdate: startdate,
+        // Add more fields as needed
+    };
+
+    // Save the new place data to Realtime Database
+    push(ref(db, "places/"), newPlace)
+        .then(function () {
+            console.log("Data saved successfully.");
+            alert("New location data submitted!");
+                // Fetch data from the Realtime Database
+        fetchData();
+        })
+        .catch(function (error) {
+            console.error("Error saving data: ", error);
+            alert("Failed to submit location data.");
+        });
+});
+
+
+window.fetchData = function() {
+    const placesRef = ref(db, 'places/');
+    const placesQuery = query(placesRef);
+  
+    onValue(placesQuery, (snapshot) => {
+      const data = snapshot.val();
+      console.log('Data fetched from Firebase:', data);
+      const placesArray = [];
+  
+      for (const key in data) {
+        placesArray.push(data[key]);
+      }
+  
+      fetchAndDisplayData(placesArray);
+    }, (error) => {
+      console.error('Error fetching data from Firebase:', error);
+    });
+  };
+
+function fetchAndDisplayData(places) {
+    GetMap(places);
+  }
+  
