ed1a44742a3f218b529ea39f5b8ddcbd8ef32816
CHarles <calsoufi@microsoft.com>
2023-03-24 21:28:22 +0100

    Update html to fetch json array

--- scriptupdatejson.py
+++ scriptupdatejson.py
@@ -1,22 +1,11 @@
 import json
-from datetime import datetime
+import uuid
 
-
-# Load the original JSON file
-with open('places.json', 'r') as f:
+with open('places.json') as f:
     data = json.load(f)
 
-# Loop through the array and update the date format for each entry
-for entry in data:
-    # Extract the original date string
-    original_date = entry['startdate']
-    
-    # Convert the date string to the new format
-    new_date = datetime.strptime(entry['startdate'], '%d.%m.%y').strftime('%m.%Y')
-    
-    # Update the entry with the new date format
-    entry['startdate'] = new_date
+for item in data:
+    item['guid'] = str(uuid.uuid4())
 
-# Write the updated JSON data to a new file
 with open('places.json', 'w') as f:
-    json.dump(data, f, indent=2)
+    json.dump(data, f, indent=4)
