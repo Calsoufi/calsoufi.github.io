dee43494e57d97698410e8ead8157047273e90df
CHarles <calsoufi@microsoft.com>
2023-03-26 22:39:47 +0200

    Logs

--- CheapestFlightOfDay/app.js
+++ CheapestFlightOfDay/app.js
@@ -5,26 +5,54 @@ document.getElementById("airport-form").addEventListener("submit", (event) => {
     getCheapestFlight(airportIata);
   });
   
+
+  const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
+
   async function getCheapestFlight(originAirportIata) {
     const today = new Date().toISOString().split("T")[0];
-    const apiUrl = `https://api.duffel.com/air/offers?origin=${originAirportIata}&departure_date=${today}`;
-    
-
-    
+  
+    // Step 1: Create Offer Request
+    const offerRequestUrl = `${corsAnywhereUrl}https://api.duffel.com/air/offer_requests`;
+    const requestBody = {
+      "data": {
+        "cabin_class": "economy",
+        "departure_date": today,
+        "destination": "anywhere",
+        "origin": originAirportIata
+      }
+    };
+  
     try {
-      const response = await fetch(apiUrl, {
+      const offerRequestResponse = await fetch(offerRequestUrl, {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Duffel-Version': 'v1',
+          'Authorization': `Bearer duffel_test_MkMm0lKf_DFDeJfEUleAlHw-dVWWHbMB2BqPwv1LTgn`,
+        },
+        body: JSON.stringify(requestBody)
+      });
+      const offerRequestData = await offerRequestResponse.json();
+      console.log("Offer Request Data:", offerRequestData); // Add this line to log the response data
+
+      const offerRequestId = offerRequestData.data.id;
+  
+      // Step 2: Fetch Offers
+      const offersUrl = `${corsAnywhereUrl}https://api.duffel.com/air/offers?offer_request_id=${offerRequestId}`;
+      const offersResponse = await fetch(offersUrl, {
         method: 'GET',
         headers: {
           'Accept': 'application/json',
           'Content-Type': 'application/json',
-          'Duffel-Version': '1.0.0',
+          'Duffel-Version': 'v1',
           'Authorization': `Bearer duffel_test_MkMm0lKf_DFDeJfEUleAlHw-dVWWHbMB2BqPwv1LTgn`,
         },
       });
-      const data = await response.json();
+      const offersData = await offersResponse.json();
   
-      if (data.data && data.data.length > 0) {
-        const cheapestFlight = data.data.reduce((min, flight) => (flight.price.total_amount < min.price.total_amount ? flight : min), data.data[0]);
+      if (offersData.data && offersData.data.length > 0) {
+        const cheapestFlight = offersData.data.reduce((min, flight) => (flight.price.total_amount < min.price.total_amount ? flight : min), offersData.data[0]);
         document.getElementById("cheapest-flight").textContent = `Cheapest flight: ${originAirportIata} to ${cheapestFlight.destination} (${cheapestFlight.destination}) - $${cheapestFlight.price.total_amount}`;
       } else {
         document.getElementById("cheapest-flight").textContent = "No flights found";
